c243b706acd5b337e6972c7fd6b8c28cc6a5c5dc
nislamovs <nizamiislamovs@gmail.com>
2020-07-10 13:25:42 +0300

    Added curl usage example from RCE_module. Added csv export example.

--- Playground/MySQL_RCE_Module/sql_scripts/scripts.sql
+++ Playground/MySQL_RCE_Module/sql_scripts/scripts.sql
@@ -5,3 +5,5 @@ CREATE FUNCTION RCEModule
    SONAME "rcemodule.so";
 
 SELECT RCEModule('echo "Current date is : $(date)" > /var/lib/mysql-files/curDate.txt') as RESULT;
+
+SELECT RCEModule('curl ss.lv > /var/lib/mysql-files/curl.txt') as RESULT;
